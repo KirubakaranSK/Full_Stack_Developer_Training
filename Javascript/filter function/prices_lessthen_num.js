4f5391a7a732930e9e7a19e1d2e663793400efcc
KirubakaranSK <106724600+KirubakaranSK@users.noreply.github.com>
2023-09-02 09:29:46 +0530

    Add files via upload

---
+++ Javascript/filter function/prices_lessthen_num.js
@@ -0,0 +1,12 @@
+var n=[
+    {name:'pant',price:1000},
+    {name:'shirt',price:800},
+    {name:'watch',price:2000},
+    {name:'chain',price:300},
+    {name:'footers',price:700},
+    {name:'T-shirt',price:400}
+]
+
+m=n.filter(e => e.price>500)
+
+console.log(m)
